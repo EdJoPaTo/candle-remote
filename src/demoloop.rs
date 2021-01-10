420dd0122b697c31250459f6c4bbd4e85deb6dd4
EdJoPaTo <git-commit-with-a-funny-long-email-address-everyone-hates-as-it-is-too-long@edjopato.de>
2021-01-10 13:51:10 +0100

    feat(demoloop): dont repeat same color in row

--- src/demoloop.rs
+++ src/demoloop.rs
@@ -4,12 +4,20 @@ use std::thread::sleep;
 use std::time::Duration;
 
 const INCREASE_HEIGHT_DURATION: Duration = Duration::from_millis(100);
+const MIN_HUE_SHIFT: f32 = 45.0;
+
+fn get_next_hue(last: f32) -> f32 {
+    let random_part = rand::random::<f32>() * (360.0 - (2.0 * MIN_HUE_SHIFT));
+    let next = last + random_part + MIN_HUE_SHIFT;
+    f32::round(next % 360.0)
+}
 
 pub fn do_stuff(sender: &mut Sender, burntime: u64) {
     let duration = Duration::from_millis(burntime);
+    let mut hue = 0.0;
 
     loop {
-        let hue = f32::round(rand::random::<f32>() * 360.0);
+        hue = get_next_hue(hue);
 
         println!("new candle... hue: {}", hue);
 
